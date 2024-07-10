5b1b7e1367505d871a62497c52793513e845d12e
Anish-Codeth <codeth690@gmail.comm>
2024-07-10 23:21:29 +0545

    another approach anf into queues

--- modules/download.js
+++ modules/download.js
@@ -1,5 +1,6 @@
 import net from 'node:net'
 import {Buffer} from 'node:buffer'
+import Pieces from './Pieces.js'
 import {getpeers} from './tracker.js'
 import 'dotenv/config'
 import {message} from './message.js'
@@ -8,112 +9,101 @@ import { createFile, saveBuffer } from './filesave.js'
 import { Block2Pieces, JobQueue } from './requestQueue.js'
 
 
-function downlaod(peer,torrent,block2Pieces,jobQueue,file){
 
-const socket = new net.Socket();
-socket.on('error', (err)=>{console.log('err')});
-socket.connect(peer.port, peer.ip, () => {
-    // 1
+const peerDownload = torrent => {
+  tracker.getPeers(torrent, peers => {
+    const pieces = new Pieces(torrent.info.pieces.length / 20);
+    peers.forEach(peer => download(peer, torrent,pieces));
+  });
+};
+
+function download(peer, torrent,pieces) {
+  const socket = new net.Socket();
+  socket.on('error', console.log);
+  socket.connect(peer.port, peer.ip, () => {
     socket.write(message.buildHandshake(torrent));
-});
+  });
+  const queue={queue:[],choked:true} //as we are choked at first
+  onWholeMsg(socket, msg => msgHandler(msg, socket,pieces,queue));
+}
 
-onWholeMsg(socket, msg => msgHandler(msg, socket,block2Pieces,jobQueue,file));
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+
+  socket.on('data', recvBuf => {
+    const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
+  });
+}
+
+function msgHandler(msg, socket) {
+  if (isHandshake(msg)) {
+    socket.write(message.buildInterested());
+  } else {
+    const m = message.parse(msg);
+
+    if (m.id === 0) chokeHandler(socket);
+    if (m.id === 1) unchokeHandler(socket,pieces,queue);
+    if (m.id === 4) haveHandler(m.payload);
+    if (m.id === 5) bitfieldHandler(m.payload);
+    if (m.id === 7) pieceHandler(m.payload);
+  }
+}
+
+function isHandshake(msg) {
+  return msg.length === msg.readUInt8(0) + 49 &&
+         msg.toString('utf8', 1, 20) === 'BitTorrent protocol';
 }
 
 
-//to get whoole message
-const onWholeMsg=(socket,callback)=>{
-let savedBuf=Buffer.alloc(0);
-let handshake=true
+function requestPiece(socket, pieces, peerQueue) {
+if (peerQueue.choked)
+    return false
 
-socket.on('data',recvBuf=>{
-    const msgLen=()=> handshake?savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) +4;
-    savedBuf=Buffer.concat([savedBuf,recvBuf])
-
-    while(savedBuf.length>=4 && savedBuf.length>=msgLen())
+while(peerQueue.queue.length>0)
+{
+    const pieceIndex=peerQueue.queue.shift()
+    if(pieces.needed(pieceIndex))
     {
-        callback(savedBuf.slice(0,msgLen()))
-        savedBuf=savedBuf.slice(msgLen())
-        handshake=false
-    }
-})
-}
-
-
-//making tcp connection for all peers
-const peerDownload=(filename)=>{getpeers(filename,(peers)=>{
-    const torrent=readTorrentFile(filename)
-    const block2Pieces=new Block2Pieces(torrent)
-    const jobQueue=new JobQueue(torrent)
-    const file=createFile(torrent)
-peers.forEach(peer=>downlaod(peer,torrent,block2Pieces,jobQueue,file));
-})
-}
-
-let count=0;
-//messagehandler
-function msgHandler(msg,socket,block2Pieces,jobQueue,file){
-    if (isHandshake(msg)){
-        socket.write(message.buildInterested()); //it sends choke,have etc
-    }
-    else{
-        const m = message.parse(msg)
-        // console.log(m)
-
-        if (m.id === 0) chokeHandler(socket);
-        // if (m.id === 1) unchokeHandler();
-        if (m.id === 4) haveHandler(socket,m.payload,block2Pieces,jobQueue);
-        if (m.id === 5) bitfieldHandler(m.payload);
-        if (m.id === 7) pieceHandler(m.payload,block2Pieces,jobQueue,file);
+        socket.write(message.buildRequest(pieceIndex))
+        pieces.addRequested(pieceIndex)
+        break
     }
 }
-
-
-function isHandshake(msg){
-    return msg.length==msg.readUInt8(0) + 49 && msg.toString('utf8',1,20) == 'BitTorrent protocol'
-}
-
+  }
 
 function chokeHandler(socket) {
 socket.end()
 }
 
-
-//for have handler
-function haveHandler(socket,payload,block2Pieces,jobQueue) {
-const index=payload.readUInt32BE(0)
-const blocks=block2Pieces.getBlockPieces(index)
-blocks.map((block)=>{
-    jobQueue.addJob(block)
-})
-const nextRequest=jobQueue.nextRequest()
-
-if(nextRequest)
-requestNextPiece(socket,nextRequest)
-
-if(jobQueue.finished())
-socket.end()
- }
-
-
- //for requesting next piece--make it callback priority
- function requestNextPiece(socket,payload){
-    console.log(payload)
-    socket.write(message.buildRequest(payload))
- }
-
-//after getting piece
-function pieceHandler(payload,block2Pieces,jobQueue,file){
-    console.log('getted',payload)
-jobQueue.addReceived(payload)
-saveBuffer(payload,block2Pieces.getPieceLength(),file)
+function unchokeHandler(socket,pieces,peerQueue) {
+peerQueue.choked=false
+requestPiece(socket,pieces,peerQueue)
 }
 
-function bitfieldHandler(payload)
-{
-    console.log(payload)
+function haveHandler(payload, socket, requested, peerQueue) {
+    const pieceIndex = payload.readUInt32BE(0);
+    peerQueue.push(pieceIndex)
+    if (peerQueue.length==1)
+    {
+        requestPiece(socket,requested,peerQueue)
+    }
 }
 
+function bitfieldHandler() {
+  // ...
+}
+
+function pieceHandler() {
+  // ...
+}
     
 export{
     peerDownload
@@ -121,3 +111,4 @@ export{
 
 
 
+/
